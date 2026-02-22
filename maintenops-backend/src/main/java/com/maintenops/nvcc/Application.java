a0cca263a2cf49689fad3c1cc120ef3442982669
Neeraj Kharde <neerajkharde7@gmail.com>
2026-02-22 15:44:34 +0530

    test

--- maintenops-backend/src/main/java/com/maintenops/nvcc/Application.java
+++ maintenops-backend/src/main/java/com/maintenops/nvcc/Application.java
@@ -12,7 +12,6 @@ import com.maintenops.nvcc.services.DepartmentService;
 import com.maintenops.nvcc.services.UserService;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
